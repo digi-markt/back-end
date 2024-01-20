224657cd13e3b110a1fbab3d20294652e40cb326
13havin-fd <bhavin-mansukh.vadhiya@informatik.hs-fulda.de>
2024-01-20 15:21:48 +0100

    Feature: Configured Websocket and processing incoming events from socket

---
+++ src/main/java/com/hochschule/digimarkt/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.hochschule.digimarkt.model;
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/hochschule/digimarkt/services/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.hochschule.digimarkt.services;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200", "http://13.51.149.52", "http://digimarkt.online", "https://digimarkt.online", "http://digimarkt.shop", "https://digimarkt.shop").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
